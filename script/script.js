be1125c7c2e35ccdaa8d05425580af8d3cd73be8
vand458 <vand458@gmail.com>
2019-07-16 22:52:14 -0400

    added episodes, update formatting

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ var data = {
         "Described as 'A love letter to the ’80s classics that captivated a generation,' \"Stranger Things\" is set in 1983 in Indiana, \"where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.\"",
         "Despite the fact that none of the series was filmed in Durham or North Carolina (it was filmed in Georgia), a ton of references to the region are hidden in the show."
     ],
-    "locations" : [
+    "locations": [
         "Kerley Road, Durham",
         "Loch Ora",
         "Eno River",
@@ -18,8 +18,7 @@ var data = {
         "text": "We have Mirkwood which is, of course, at the intersection of Cornwallis and Kerley, which is really close to where we grew up",
         "author": "Matt Duffer"
     },
-    "gallery": [
-        {
+    "gallery": [{
             "src": "https://vignette.wikia.nocookie.net/strangerthings8338/images/6/6b/The_Vanishing_of_Will_Byers_S01-E01_SS_005.png/revision/latest/scale-to-width-down/620",
             "text": "Intersection of Cornwallis and Kerley"
         },
@@ -37,8 +36,7 @@ var data = {
             "text": "Map of Hawkins"
         }
     ],
-    "episode-list": [
-        {
+    "episode-list": [{
             "season": 2,
             "name": "Chapter Nine: The Gate",
             "rating": 9.4
@@ -126,43 +124,117 @@ var data = {
     ]
 }
 
-	document.getElementById("heading").innerHTML=data.heading;
-	document.getElementById("description").innerHTML=data.description;
-	document.getElementById("snippet1").innerHTML=data.snippets[0];
-	document.getElementById("snippet2").innerHTML=data.snippets[1];
-    document.getElementById("snippet3").innerHTML=data.snippets[2];
-	document.getElementById("video-embed").src=data['video-embed'];	
-	
-	document.getElementById("quoteText").innerHTML=data.quote.text;
-	document.getElementById("quoteAuthor").innerHTML=data.quote.author;
-	document.getElementById("locationKerleyCornwally").alt=data.locations[0];
-	document.getElementById("locationLochNora").alt=data.locations[0];
-	document.getElementById("locationLochNora").innerHTML=data.locations[1];
-	document.getElementById("locationKerleyCornwally").src=data.gallery[0].src;
-	document.getElementById("trickOrTreatLochNoraImg").src=data.gallery[1].src;
-	document.getElementById("trickOrTreatLochNora").innerHTML=data.gallery[1].text;
-	document.getElementById("drawingEnoJordanImg").src=data.gallery[2].src;
-	document.getElementById("drawingEnoJordan").innerHTML=data.gallery[2].text;
-	document.getElementById("mapHawkinsImg").src=data.gallery[3].src;
-	document.getElementById("mapHawkins").innerHTML=data.gallery[3].text;
-	
-//episode list 
-	document.getElementById("episodeLi1Season").innerHTML=data['episode-list'][0].season;
-	document.getElementById("episodeLi1Name").innerHTML= data['episode-list'][0].name;
-	document.getElementById("episodeLi1Rating").innerHTML=data['episode-list'][0].rating;
-	
-/* TODO fix  blocked by CORS policy: error! 
-	
-	var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myObj.name;
-  }
-};
-xmlhttp.open("GET", "data/en_US.json", true);
-xmlhttp.send();
-*/	
+document.getElementById("heading").innerHTML = data.heading;
+document.getElementById("description").innerHTML = data.description;
+document.getElementById("snippet1").innerHTML = data.snippets[0];
+document.getElementById("snippet2").innerHTML = data.snippets[1];
+document.getElementById("snippet3").innerHTML = data.snippets[2];
+document.getElementById("video-embed").src = data['video-embed'];
+
+document.getElementById("quoteText").innerHTML = data.quote.text;
+document.getElementById("quoteAuthor").innerHTML = data.quote.author;
+document.getElementById("locationKerleyCornwally").alt = data.locations[0];
+document.getElementById("locationLochNora").alt = data.locations[0];
+document.getElementById("locationLochNora").innerHTML = data.locations[1];
+document.getElementById("locationKerleyCornwally").src = data.gallery[0].src;
+document.getElementById("trickOrTreatLochNoraImg").src = data.gallery[1].src;
+document.getElementById("trickOrTreatLochNora").innerHTML = data.gallery[1].text;
+document.getElementById("drawingEnoJordanImg").src = data.gallery[2].src;
+document.getElementById("drawingEnoJordan").innerHTML = data.gallery[2].text;
+document.getElementById("mapHawkinsImg").src = data.gallery[3].src;
+document.getElementById("mapHawkins").innerHTML = data.gallery[3].text;
+
+//episode list
+document.getElementById("episodeLi2_9Season").innerHTML = data['episode-list'][0].season;
+document.getElementById("episodeLi2_9Name").innerHTML = data['episode-list'][0].name;
+document.getElementById("episodeLi2_9Rating").innerHTML = data['episode-list'][0].rating;
+
+document.getElementById("episodeLi2_8Season").innerHTML = data['episode-list'][1].season;
+document.getElementById("episodeLi2_8Name").innerHTML = data['episode-list'][1].name;
+document.getElementById("episodeLi2_8Rating").innerHTML = data['episode-list'][1].rating;
+
+document.getElementById("episodeLi1_8Season").innerHTML = data['episode-list'][2].season;
+document.getElementById("episodeLi1_8Name").innerHTML = data['episode-list'][2].name;
+document.getElementById("episodeLi1_8Rating").innerHTML = data['episode-list'][2].rating;
+
+document.getElementById("episodeLi2_6Season").innerHTML = data['episode-list'][3].season;
+document.getElementById("episodeLi2_6Name").innerHTML = data['episode-list'][3].name;
+document.getElementById("episodeLi2_6Rating").innerHTML = data['episode-list'][3].rating;
+
+document.getElementById("episodeLi2_8Season").innerHTML = data['episode-list'][4].season;
+document.getElementById("episodeLi2_8Name").innerHTML = data['episode-list'][4].name;
+document.getElementById("episodeLi2_8Rating").innerHTML = data['episode-list'][4].rating;
+
+document.getElementById("episodeLi1_7Season").innerHTML = data['episode-list'][5].season;
+document.getElementById("episodeLi1_7Name").innerHTML = data['episode-list'][5].name;
+document.getElementById("episodeLi1_7Rating").innerHTML = data['episode-list'][5].rating;
+
+document.getElementById("episodeLi2_5Season").innerHTML = data['episode-list'][6].season;
+document.getElementById("episodeLi2_5Name").innerHTML = data['episode-list'][6].name;
+document.getElementById("episodeLi2_5Rating").innerHTML = data['episode-list'][6].rating;
+
+document.getElementById("episodeLi1_3Season").innerHTML = data['episode-list'][7].season;
+document.getElementById("episodeLi1_3Name").innerHTML = data['episode-list'][7].name;
+document.getElementById("episodeLi1_3Rating").innerHTML = data['episode-list'][7].rating;
+
+document.getElementById("episodeLi1_4Season").innerHTML = data['episode-list'][8].season;
+document.getElementById("episodeLi1_4Name").innerHTML = data['episode-list'][8].name;
+document.getElementById("episodeLi1_4Rating").innerHTML = data['episode-list'][8].rating;
+
+document.getElementById("episodeLi1_6Season").innerHTML = data['episode-list'][9].season;
+document.getElementById("episodeLi1_6Name").innerHTML = data['episode-list'][9].name;
+document.getElementById("episodeLi1_6Rating").innerHTML = data['episode-list'][9].rating;
+
+document.getElementById("episodeLi2_4Season").innerHTML = data['episode-list'][1].season;
+document.getElementById("episodeLi2_4Name").innerHTML = data['episode-list'][1].name;
+document.getElementById("episodeLi2_4Rating").innerHTML = data['episode-list'][1].rating;
+
+document.getElementById("episodeLi2_3Season").innerHTML = data['episode-list'][10].season;
+document.getElementById("episodeLi2_3Name").innerHTML = data['episode-list'][10].name;
+document.getElementById("episodeLi2_3Rating").innerHTML = data['episode-list'][10].rating;
+
+document.getElementById("episodeLi1_5Season").innerHTML = data['episode-list'][11].season;
+document.getElementById("episodeLi1_5Name").innerHTML = data['episode-list'][11].name;
+document.getElementById("episodeLi1_5Rating").innerHTML = data['episode-list'][11].rating;
+
+document.getElementById("episodeLi1_1Season").innerHTML = data['episode-list'][12].season;
+document.getElementById("episodeLi1_1Name").innerHTML = data['episode-list'][12].name;
+document.getElementById("episodeLi1_1Rating").innerHTML = data['episode-list'][12].rating;
+
+document.getElementById("episodeLi2_2Season").innerHTML = data['episode-list'][13].season;
+document.getElementById("episodeLi2_2Name").innerHTML = data['episode-list'][13].name;
+document.getElementById("episodeLi2_2Rating").innerHTML = data['episode-list'][13].rating;
+
+document.getElementById("episodeLi1_2Season").innerHTML = data['episode-list'][14].season;
+document.getElementById("episodeLi1_2Name").innerHTML = data['episode-list'][14].name;
+document.getElementById("episodeLi1_2Rating").innerHTML = data['episode-list'][14].rating;
+
+document.getElementById("episodeLi2_1Season").innerHTML = data['episode-list'][15].season;
+document.getElementById("episodeLi2_1Name").innerHTML = data['episode-list'][15].name;
+document.getElementById("episodeLi2_1Rating").innerHTML = data['episode-list'][15].rating;
+
+document.getElementById("episodeLi2_7Season").innerHTML = data['episode-list'][16].season;
+document.getElementById("episodeLi2_7Name").innerHTML = data['episode-list'][16].name;
+document.getElementById("episodeLi2_7Rating").innerHTML = data['episode-list'][16].rating;
+
+
+
+/* TODO fix  blocked by CORS policy: error!
+
+var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.json');
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)) {
+		var items = JSON.parse(request.responseText);
+		console.log(items);
+	}
+}
+request.send();
+*/
 //console.log(data.description);
 //console.log(data["episode-list"]);
-
